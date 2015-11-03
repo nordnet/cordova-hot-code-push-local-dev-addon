baf922c910b1ecdad0d5468009c8998090016250
Nikolay Demyankov <nikolay.demyankov@nordnet.se>
2015-11-03 15:21:16 +0100

    Android. Added socket connection.

--- src/android/src/com/nordnetab/chcp/localdev/HotCodePushLocalDevPlugin.java
+++ src/android/src/com/nordnetab/chcp/localdev/HotCodePushLocalDevPlugin.java
@@ -1,19 +1,18 @@
 package com.nordnetab.chcp.localdev;
 
-import org.apache.cordova.CallbackContext;
-import org.apache.cordova.ConfigXmlParser;
-import org.apache.cordova.CordovaArgs;
+import android.util.Log;
+
+import com.nordnetab.chcp.main.config.ChcpXmlConfig;
+
 import org.apache.cordova.CordovaInterface;
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CordovaWebView;
-import org.apache.cordova.PluginResult;
-import org.json.JSONException;
-import org.json.JSONObject;
 
-import java.io.File;
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
+
+import io.socket.client.IO;
+import io.socket.client.Socket;
+import io.socket.emitter.Emitter;
 
 /**
  * Created by Nikolay Demyankov on 23.07.15.
@@ -22,11 +21,108 @@ import java.util.List;
  */
 public class HotCodePushLocalDevPlugin extends CordovaPlugin {
 
+    private Socket devSocket;
+    private ChcpXmlConfig chcpXmlConfig;
+
     // region Plugin lifecycle
 
     @Override
     public void initialize(final CordovaInterface cordova, final CordovaWebView webView) {
         super.initialize(cordova, webView);
 
+        parseCordovaConfigXml();
     }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        connectToLocalDevSocket();
+    }
+
+    @Override
+    public void onStop() {
+        disconnectFromLocalDevSocket();
+
+        super.onStop();
+    }
+
+    // endregion
+
+    // region Private API
+
+    /**
+     * Read hot-code-push plugin preferences from cordova config.xml
+     *
+     * @see ChcpXmlConfig
+     */
+    private void parseCordovaConfigXml() {
+        if (chcpXmlConfig != null) {
+            return;
+        }
+
+        chcpXmlConfig = ChcpXmlConfig.loadFromCordovaConfig(cordova.getActivity());
+    }
+
+    // endregion
+
+    // region Local development socket
+
+    /**
+     * Connect to local server to listen for update in real time.
+     * Called only when local development mode is enabled in config.xml
+     */
+    private void connectToLocalDevSocket() {
+        if (!chcpXmlConfig.getDevelopmentOptions().isEnabled()) {
+            return;
+        }
+
+        try {
+            URL serverURL = new URL(chcpXmlConfig.getConfigUrl());
+            String socketUrl = serverURL.getProtocol() + "://" + serverURL.getAuthority();
+
+            devSocket = IO.socket(socketUrl);
+            devSocket.on(Socket.EVENT_CONNECT, new Emitter.Listener() {
+
+                @Override
+                public void call(Object... args) {
+                    Log.d("CHCP", "Socket connected");
+                }
+
+            }).on("release", new Emitter.Listener() {
+
+                @Override
+                public void call(Object... args) {
+                    Log.d("CHCP", "New Release is available");
+                    //fetchUpdate(null);
+                }
+
+            }).on(Socket.EVENT_DISCONNECT, new Emitter.Listener() {
+
+                @Override
+                public void call(Object... args) {
+                    Log.d("CHCP", "Socket disonnected");
+                }
+
+            });
+            devSocket.connect();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Disconnect from local server.
+     */
+    private void disconnectFromLocalDevSocket() {
+        if (devSocket == null) {
+            return;
+        }
+
+        devSocket.close();
+        devSocket.off();
+        devSocket = null;
+    }
+
+    // endregion
 }
