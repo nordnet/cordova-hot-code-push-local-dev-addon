ff526cc80a8382da8f89b137f3a2ab31bd292638
Nikolay Demyankov <nikolay.demyankov@nordnet.se>
2016-04-22 16:45:11 +0200

    Simplified before_plugin_install hook: now node modules will be installed in plugins folder instead of the projects root folder.

--- scripts/beforePluginInstallHook.js
+++ scripts/beforePluginInstallHook.js
@@ -3,121 +3,13 @@ Hook is executed when plugin is added to the project.
 It will check all necessary module dependencies and install the missing ones locally.
 */
 
-var exec = require('child_process').exec,
-  path = require('path'),
-  fs = require('fs'),
-  modules = ['read-package-json'],
-  INSTALLATION_FLAG_FILE_NAME = '.installed',
-  packageJsonFilePath;
+var path = require('path');
+var fs = require('fs');
+var spawnSync = require('child_process').spawnSync;
+var pluginNpmDependencies = require('../package.json').dependencies;
+var INSTALLATION_FLAG_FILE_NAME = '.npmInstalled';
 
-// region NPM specific
-
-/**
- * Discovers module dependencies in plugin's package.json and installs them.
- */
-function installModulesFromPackageJson() {
-  var readJson = require('read-package-json');
-  readJson(packageJsonFilePath, console.error, false, function(err, data) {
-    if (err) {
-      printMsg('Can\'t read package.json file: ' + err);
-      return;
-    }
-
-    var dependencies = data['dependencies'];
-    if (dependencies) {
-      for (var module in dependencies) {
-        modules.push(module);
-      }
-      installRequiredNodeModules(function() {
-        printMsg('All dependency modules are installed.');
-      });
-    }
-  });
-}
-
-/**
- * Check if node package is installed.
- *
- * @param {String} moduleName
- * @return {Boolean} true if package already installed
- */
-function isNodeModuleInstalled(moduleName) {
-  var installed = true;
-  try {
-    var module = require(moduleName);
-  } catch (err) {
-    installed = false;
-  }
-
-  return installed;
-}
-
-/**
- * Install node module locally.
- * Basically, it runs 'npm install module_name'.
- *
- * @param {String} moduleName
- * @param {Callback(error)} callback
- */
-function installNodeModule(moduleName, callback) {
-  if (isNodeModuleInstalled(moduleName)) {
-    callback(null);
-    return;
-  }
-  printMsg('Can\'t find module ' + moduleName + ', running npm install');
-
-  var cmd = 'npm install -D ' + moduleName;
-  exec(cmd, function(err, stdout, stderr) {
-    callback(err);
-  });
-}
-
-/**
- * Install all required node packages.
- */
-function installRequiredNodeModules(callback) {
-  if (modules.length == 0) {
-    callback();
-    return;
-  }
-
-  var moduleName = modules.shift();
-  installNodeModule(moduleName, function(err) {
-    if (err) {
-      printMsg('Failed to install module ' + moduleName + ':' + err);
-      return;
-    }
-
-    printMsg('Module ' + moduleName + ' is installed');
-    installRequiredNodeModules(callback);
-  });
-}
-
-// endregion
-
-// region Logging
-
-function printHeader(msg) {
-  console.log(msg);
-}
-
-function printMsg(msg) {
-  console.log('    ' + msg);
-}
-
-// endregion
-
-// region Private API
-
-/**
- * Perform initialization before any execution.
- *
- * @param {Object} ctx - cordova context object
- */
-function init(ctx) {
-  packageJsonFilePath = path.join(ctx.opts.projectRoot, 'plugins', ctx.opts.plugin.id, 'package.json');
-}
-
+// region mark that we installed npm packages
 /**
  * Check if we already executed this hook.
  *
@@ -125,15 +17,13 @@ function init(ctx) {
  * @return {Boolean} true if already executed; otherwise - false
  */
 function isInstallationAlreadyPerformed(ctx) {
-  var pathToInstallFlag = path.join(ctx.opts.projectRoot, 'plugins', ctx.opts.plugin.id, INSTALLATION_FLAG_FILE_NAME),
-    isInstalled = false;
+  var pathToInstallFlag = path.join(ctx.opts.projectRoot, 'plugins', ctx.opts.plugin.id, INSTALLATION_FLAG_FILE_NAME);
   try {
-    var content = fs.readFileSync(pathToInstallFlag);
-    isInstalled = true;
+    fs.accessSync(pathToInstallFlag, fs.F_OK);
+    return true;
   } catch (err) {
+    return false;
   }
-
-  return isInstalled;
 }
 
 /**
@@ -145,19 +35,20 @@ function createPluginInstalledFlag(ctx) {
 
   fs.closeSync(fs.openSync(pathToInstallFlag, 'w'));
 }
-
 // endregion
 
 module.exports = function(ctx) {
-  // exit if we already executed this hook once
   if (isInstallationAlreadyPerformed(ctx)) {
     return;
   }
 
-  printHeader('CHCP Local Dev add-on checking dependencies:');
+  console.log('Installing dependency packages: ');
+  console.log(JSON.stringify(pluginNpmDependencies, null, 2));
 
-  init(ctx);
-  installRequiredNodeModules(installModulesFromPackageJson);
+  var result = spawnSync('npm', ['install', '--production'], { cwd: './plugins/' + ctx.opts.plugin.id });
+  if (result.error) {
+    throw error;
+  }
 
   createPluginInstalledFlag(ctx);
 };
